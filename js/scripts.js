ff19aa90c583c760cd206ef4a34ffd7ed4364961
Marcel Legane <mlegane@gmail.com>
2015-02-03 23:32:51 +0000

    Animate chimp head on mousemove

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,65 @@
 (function($){
 
 
-    'use strict';
+   'use strict';
 
 
-    //------------------------------------------------------------------------
-    //  $Utility functions
-    //------------------------------------------------------------------------
+   var winW;
+   var winH;
+   var $head = $('.chimp__head');
+   var $headCenterX;
+   var $headCenterY;
+
+   function elSize() {
+      winW = $(window).width();
+      winH = $(window).height();
+      $headCenterX = $head.offset().left + 83;
+      $headCenterY = $head.offset().top + 72;
+   }
+   elSize();
+
+   $(window).on('resize', function() {
+      elSize();
+   });
 
 
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    }
+   //------------------------------------------------------------------------
+   //  $Kong
+   //------------------------------------------------------------------------
+
+
+   $('html').mousemove(function(e) {
+      var x = e.pageX;
+      var y = e.pageY;
+      var xFactor = ((x / $headCenterX) - 1).toFixed(2);
+      var yFactor = ((y / $headCenterY) - 1).toFixed(2);
+
+      if (xFactor < 0) {
+         yFactor = yFactor * -1;
+      }
+
+      $head.css('transform', 'rotate(' + (yFactor * 70) + 'deg)');
+   });
+
+
+   //------------------------------------------------------------------------
+   //  $Utility functions
+   //------------------------------------------------------------------------
+
+
+   function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+         var context = this, args = arguments;
+         var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+         };
+         var callNow = immediate && !timeout;
+         clearTimeout(timeout);
+         timeout = setTimeout(later, wait);
+         if (callNow) func.apply(context, args);
+      };
+   }
 
 })(jQuery);
