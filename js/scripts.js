019b13e254b4d9fe201434c956b1efeb22ec83f0
Marcel Legane <mlegane@gmail.com>
2015-02-12 09:33:00 +0000

    Add hover states, footer character

--- js/scripts.js
+++ js/scripts.js
@@ -49,23 +49,19 @@
 
 
    //------------------------------------------------------------------------
-   //  $Utility functions
+   //  $Scroll Down
    //------------------------------------------------------------------------
 
 
-   function debounce(func, wait, immediate) {
-      var timeout;
-      return function() {
-         var context = this, args = arguments;
-         var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-         };
-         var callNow = immediate && !timeout;
-         clearTimeout(timeout);
-         timeout = setTimeout(later, wait);
-         if (callNow) func.apply(context, args);
-      };
-   }
+   $('.scroll').on('click', function() {
+      var target = $(this).attr('href');
+      var targetOffset = $(target).offset().top;
+
+      $('html, body').animate({
+         scrollTop: targetOffset
+      });
+
+      return false;
+   });
 
 })(jQuery);
